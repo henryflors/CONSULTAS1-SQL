b820efc3d52f2f28fb2595f6e9d4da323b9dd2e3
HENRY FLORES <floreshenry901@gmail.com>
2025-03-16 18:57:22 -0400

    sqlzoo

--- CONSLSQLzoo.sql
+++ CONSLSQLzoo.sql
@@ -89,5 +89,111 @@ SELECT name
   FROM world
  WHERE name = capital;
 
---ERROR
+12
+--The capital of Sweden is Stockholm. Both words start with the letter 'S'.
+--Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+--You can use the function LEFT to isolate the first character.
+--You can use <> as the NOT EQUALS operator.
+SELECT name,
+       CASE WHEN continent='Europe' or continent='Asia' THEN 'Eurasia'
+            WHEN continent in ('North America','South America','Caribbean') THEN 'America'   
+            ELSE continent END
+  FROM world
+ WHERE name LIKE 'A%' or name LIKE 'B%'
+13
+--Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. They don't count because they have more than one word in the name.
+--Find the country that has all the vowels and no spaces in its name.
+--You can use the phrase name NOT LIKE '%a%' to exclude characters from your results.
+--The query shown misses countries like Bahamas and Belarus because they contain at least one 'a'
 
+SELECT name, continent, CASE
+                     WHEN continent = 'Oceania' THEN 'Australasia'
+                     WHEN continent = 'Eurasia' THEN 'Europe/Asia'
+                     WHEN name = 'Turkey' THEN 'Europe/Asia'
+    WHEN continent = 'Caribbean' AND name LIKE 'B%' then 'North America'
+    WHEN continent = 'Caribbean' THEN 'South America'    
+                 ELSE continent END
+FROM world ORDER BY name
+
+--SELECT from Nobel Tutorial
+
+1
+--Change the query shown so that it displays Nobel prizes for 1950.
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+
+2
+--Show who won the 1962 prize for literature.
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+3
+--Show the year and subject that won 'Albert Einstein' his prize.
+
+SELECT yr, subject
+  FROM nobel
+WHERE winner = 'Albert Einstein'
+4
+--Give the name of the 'peace' winners since the year 2000, including 2000.
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000
+5
+--Show all details (yr, subject, winner) of the literature prize winners for 1980 to 1989 inclusive.
+SELECT yr, subject, winner
+  FROM nobel
+WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989
+(in MySQL BETWEEN is inclusive.. this is not always the case)
+6
+--Show all details of the presidential winners:
+--Theodore Roosevelt
+--Thomas Woodrow Wilson
+--Jimmy Carter
+--Barack Obama
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter')
+7
+--Show the winners with first name John
+SELECT winner FROM nobel
+  WHERE winner LIKE 'JOHN %'
+8
+--Show the year, subject, and name of physics winners for 1980 together with the chemistry winners for 1984.
+SELECT * FROM nobel
+  WHERE yr = 1980 AND subject = 'Physics'
+     OR yr = 1984 AND subject = 'Chemistry'
+9
+--Show the year, subject, and name of winners for 1980 excluding chemistry and medicine
+SELECT * FROM nobel
+  WHERE yr = 1980
+    AND subject NOT IN ('Chemistry','Medicine')
+10
+--Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+SELECT * FROM nobel
+  WHERE yr < 1910 AND subject = 'Medicine'
+     OR yr >= 2004 AND subject = 'Literature'
+
+--Harder Questions
+11
+--Find all details of the prize won by PETER GRÜNBERG
+SELECT * FROM nobel
+  WHERE winner = 'Peter Grünberg'
+12
+--Find all details of the prize won by EUGENE O'NEILL
+SELECT * FROM nobel
+  WHERE winner = 'Eugene O''Neill'
+13
+--List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+SELECT winner, yr, subject FROM nobel
+  WHERE winner LIKE 'Sir %'
+  ORDER BY yr DESC, winner
+14
+--The expression subject IN ('chemistry','physics') can be used as a value - it will be 0 or 1.
+--Show the 1984 winners and subject ordered by subject and winner name; but list chemistry and physics last.
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject in ('Chemistry','Physics'), subject, winner
