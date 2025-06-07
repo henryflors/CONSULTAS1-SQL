ec3e5559d986ee814e14fd5dc061ee8f1ca50a69
henryflors <54456276+henryflors@users.noreply.github.com>
2025-06-06 23:10:28 -0400

    Update hackerRank.sql

--- hackerRank.sql
+++ hackerRank.sql
@@ -92,3 +92,33 @@ WHERE CITY NOT LIKE 'A%'
   AND CITY NOT LIKE 'I%'
   AND CITY NOT LIKE 'O%'
   AND CITY NOT LIKE 'U%';
+--Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+--Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+--Input Format
+--The STATION table is described as follows:
+SELECT DISTINCT CITY
+FROM STATION
+WHERE 
+    LEFT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u')
+    OR
+    RIGHT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
+--Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+--Input Format
+--The STATION table is described as follows:
+SELECT DISTINCT CITY
+FROM STATION
+WHERE 
+    LEFT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u')
+    AND
+    RIGHT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
+--Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+--Input Format
+--The STUDENTS table is described as follows:
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), ID;
+
